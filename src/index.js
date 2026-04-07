22e4abc8a23d6b3cbfd4d1f768b7d6ff86c1712a
Van-Code <19809586+Van-Code@users.noreply.github.com>
2026-04-06 17:16:33 -0700

    configure baseroute

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/streaming-app">
       <App />
     </Router>
   </React.StrictMode>,
